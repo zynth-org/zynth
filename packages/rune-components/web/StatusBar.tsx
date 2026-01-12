4c01ffb8fea13f3ad35effa327d82e5799c5523e
Ignacio Zsabo <ignacioluisr2006@gmail.com>
2026-01-12 17:18:25 -0400

    fix(web): StatusBar shim

---
+++ packages/rune-components/web/StatusBar.tsx
@@ -0,0 +1,8 @@
+/** @jsxImportSource solid-js */
+import { registerComponent } from "./utils";
+
+export const StatusBar = () => {
+  return <div style="display: none" aria-hidden="true" />;
+};
+
+registerComponent("rune-status-bar", StatusBar);
